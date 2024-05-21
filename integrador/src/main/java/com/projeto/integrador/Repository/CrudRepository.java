85676e1acd5a785ea434a57461b0703114636d79
Michelle Madeira <michellemadeira1@gmail.com>
2024-05-20 23:52:04 -0300

    projeto

---
+++ integrador/src/main/java/com/projeto/integrador/Repository/CrudRepository.java
@@ -0,0 +1,21 @@
+package com.projeto.integrador.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.integrador.usuario.Usuario;
+
+
+
+@Repository
+public interface CrudRepository extends JpaRepository<Usuario, Long> {
+	
+	//public List<Usuario> findAll();
+	//public List<Usuario> findById(Long id);
+	//public Optional<Usuario>findByResponsavel(String responsavel);*/
+	List<Usuario> findByNomeContainingIgnoreCase(String nome);
+
+}
